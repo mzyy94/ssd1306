2c5265a5b8d10a891b54969a5569e9f19ba34130
Yuki MIZUNO <mizuyuu0904@gmail.com>
2019-10-08 23:50:33 +0900

    Remove unsupported lcd types

--- src/ssd1306.h
+++ src/ssd1306.h
@@ -37,17 +37,7 @@
 
 #include "lcd/lcd_common.h"
 #include "lcd/oled_ssd1306.h"
-#include "lcd/oled_ssd1325.h"
-#include "lcd/oled_ssd1327.h"
-#include "lcd/oled_ssd1331.h"
-#include "lcd/oled_ssd1351.h"
-#include "lcd/oled_sh1106.h"
-#include "lcd/lcd_pcd8544.h"
 #include "lcd/lcd_il9163.h"
-#include "lcd/lcd_ili9341.h"
-#include "lcd/composite_video.h"
-
-#include "lcd/oled_template.h"
 
 #ifdef __cplusplus
 extern "C" {
